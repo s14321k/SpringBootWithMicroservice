0cb7c21ad329285a1b621008b5cbb29c4548faf6
sarathkumarb <silvershinesarath@gmail.com>
2024-05-10 15:04:07 +0530

    logs cleared and configs

--- demoHacker/src/main/java/com/example/demo/sellController/SellController.java
+++ demoHacker/src/main/java/com/example/demo/sellController/SellController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -67,4 +68,15 @@ public class SellController {
         });
         return errors;
     }
+    
+//    @PutMapping("/{id}")
+//    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+//    	return userRepository.findById(id)                 
+//    			.map(user -> {                     
+//    				user.setName(updatedUser.getName());                     
+//    				user.setEmail(updatedUser.getEmail());                     
+//    				// Update other fields as neededreturn userRepository.save(user);
+//    				})                 
+//    			.orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+//    }
 }
